7ccf3b434328f7b7b1eee86e5125ab6eef162089
EkaterinaO19 <ekshepa@gmail.com>
2022-05-13 14:40:19 +0700

    Init

---
+++ src/robots.js
@@ -0,0 +1,43 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+{
+    id: 2,
+    name: 'LdjGHAg',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+{
+    id: 3,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},{
+    id: 4,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+{
+    id: 5,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+{
+    id: 6,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+{
+    id: 7,
+    name: 'Ldjd Jkjks',
+    username: 'Dkd',
+    email: 'poo@mail.ru'
+},
+];
